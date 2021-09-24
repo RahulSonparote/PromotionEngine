53fd4a681c5efbeb65d4de33380486443fd90189
sonparote.harshal <sonparote.harshal@gmail.com>
2021-09-25 01:00:30 +0530

    Base project add with models and services

---
+++ ProductPromotions/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using ProductPromotions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductPromotions.Services
+{
+    public interface IProductService
+    {
+        void GetPriceByType(Product product);
+        int GetTotalPrice(List<Product> products);
+    }
+}
---
+++ ProductPromotions/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using ProductPromotions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductPromotions.Services
+{
+    public class ProductService : IProductService
+    {
+        public void GetPriceByType(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice(List<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
